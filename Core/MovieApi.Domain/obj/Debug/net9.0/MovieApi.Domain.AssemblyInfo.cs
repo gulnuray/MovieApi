f3765ea2384041e4c2ff1e3796b35ee03d06d816
Humeyra Gul Nuray <nurayhumeyragul@gmail.com>
2026-02-24 11:39:43 +0300

    Ders 8 sonu

--- Core/MovieApi.Domain/obj/Debug/net9.0/MovieApi.Domain.AssemblyInfo.cs
+++ Core/MovieApi.Domain/obj/Debug/net9.0/MovieApi.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApi.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3beb2dc1bce4aa899f305ef0dcbc07a51c53c0dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c2e099b352f9bc0c17dbc8ff3d51c9a398e873")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApi.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApi.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Infrastructure/MovieApiPersistence/obj/Debug/net9.0/MovieApiPersistence.AssemblyInfo.cs
+++ Infrastructure/MovieApiPersistence/obj/Debug/net9.0/MovieApiPersistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApiPersistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3beb2dc1bce4aa899f305ef0dcbc07a51c53c0dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c2e099b352f9bc0c17dbc8ff3d51c9a398e873")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApiPersistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApiPersistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
